48ca5e989ffe53cfac5970c2ae6d9b32aca5acaf
elaaarfaoui <elaa.arfaoui1@gmail.com>
2021-02-11 11:42:31 +0100

    first commit

---
+++ baby-steps.js
@@ -0,0 +1,13 @@
+
+function sum (a,b,c) {  
+
+    return (a+b+c)
+}
+
+
+let imputOne=Number(process.argv[2]);
+let imputTwo=Number(process.argv[3]);
+let imputThree=Number(process.argv[4]);
+
+console.log( sum(imputOne,imputTwo,imputThree))
+
